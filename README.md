6ff1f9270822cb51f0130fb7173d8d7a1ba80240
Julian Simpson <simpsonjulian@gmail.com>
2023-04-16 16:37:30 +1200

    Manage project settings in Terraform
    * Added TF for ADO
    * Created Azure CR for ADO
    * Created service connection for CR
    * Added Environments in TF

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # pipeline-playpen
 Pipeline Playpen
+
+This is example CI/CD pipeline in Azure DevOps, with source in GitHub.
+
+## It's 2023, people understand CI/CD, right?
+
+You'd be surprised.  In my day job we're seeing that people use legacy features of ADO, to their projects detriment.
+
+## What makes a CI/CD pipeline?
+
+* One YAML file to define the workflow from build to production.  By all means, break that up into different files or use templates, but there should be one workflow to deploy one artifact to production.
+* Stages per environment.  Your pipeline should run all the stages.  You need to fix the issues in earlier stages.  Flip the "Disable stage chooser" switch.
+* Validation after deployment.
+
+## What isn't a CI/CD pipeline?
+
+* Release pipelines. They are deprecated and you should flip the "Disable creation of classic build and classic release pipelines" switch at the Organization level.  Deployment is the entire team's concern.
+* More than 1 orchestration tool.
+
+# Tools in this example
+* Node, NPM, expressJS for the "app"
+* Docker for containerisation
+* Still deciding on container runtime
+* GitHub Advanced Security for SAST, with extra Trivy
+* 
--- main.tf
+++ main.tf
@@ -4,9 +4,65 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "=3.52.0"
     }
+    azuredevops = {
+      source  = "microsoft/azuredevops"
+      version = ">=0.1.0"
+    }
   }
 }
 
+# ADO resources
+
+resource "azuredevops_project" "project" {
+  name        = "Playpen"
+  description = "CI/CD research"
+  features = {
+    "testplans" = "disabled"
+    "artifacts" = "disabled"
+  }
+  work_item_template = "Basic"
+}
+
+resource "azuredevops_project_pipeline_settings" "example" {
+  project_id = azuredevops_project.project.id
+
+  enforce_job_scope                    = true
+  enforce_referenced_repo_scoped_token = true
+  enforce_settable_var                 = true
+  publish_pipeline_metadata            = false
+  status_badges_are_private            = true
+}
+
+resource "azuredevops_environment" "Testing" {
+  project_id = azuredevops_project.project.id
+  name       = "Test Environment"
+}
+
+resource "azuredevops_environment" "Production" {
+  project_id = azuredevops_project.project.id
+  name       = "Production Environment"
+}
+
+resource "azuredevops_check_business_hours" "example" {
+  project_id           = azuredevops_project.project.id
+  display_name         = "Managed by Terraform"
+  target_resource_id   = azuredevops_environment.Production.id
+  target_resource_type = "environment"
+  start_time           = "07:00"
+  end_time             = "15:30"
+  time_zone            = "New Zealand Standard Time"
+  monday               = true
+  tuesday              = true
+  wednesday            = true
+  thursday             = true
+  friday               = true
+}
+
+variable "subscription_id" {
+  
+}
+
+# Azure Stuff
 provider "azurerm" {
   subscription_id = var.subscription_id
   features {
@@ -20,41 +76,27 @@ resource "azurerm_resource_group" "test" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "registry"
+  name                = "builddoctorplaypen"
   resource_group_name = azurerm_resource_group.test.name
   location            = azurerm_resource_group.test.location
   sku                 = "Basic"
   admin_enabled       = false
 }
 
-
-resource "azurerm_log_analytics_workspace" "central" {
-  name                = "acctest-01"
+resource "azurerm_log_analytics_workspace" "logs" {
+  name                = "pipeline-01"
   location            = azurerm_resource_group.test.location
   resource_group_name = azurerm_resource_group.test.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 }
 
-resource "azurerm_container_app_environment" "test" {
-  name                       = "Test-Environment"
-  location                   = azurerm_resource_group.test.location
-  resource_group_name        = azurerm_resource_group.test.name
-  log_analytics_workspace_id = azurerm_log_analytics_workspace.central.id
+# ADO stuff that consumes Azure Stuff
+
+resource "azuredevops_serviceendpoint_dockerregistry" "repo" {
+  project_id            = azuredevops_project.project.id
+  service_endpoint_name = "Azure Container Repo"
+  docker_registry       = azurerm_container_registry.acr.login_server
+  registry_type         = "Others"
 }
 
-resource "azurerm_container_app" "example" {
-  name                         = "example-app"
-  container_app_environment_id = azurerm_container_app_environment.test.id
-  resource_group_name          = azurerm_resource_group.test.name
-  revision_mode                = "Single"
-
-  template {
-    container {
-      name   = "examplecontainerapp"
-      image  = "mcr.microsoft.com/azuredocs/containerapps-helloworld:latest"
-      cpu    = 0.25
-      memory = "0.5Gi"
-    }
-  }
-}
