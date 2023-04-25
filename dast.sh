6322ac0c1c89ac49a773881877631a94431e31d7
Julian Simpson <simpsonjulian@gmail.com>
2023-04-25 16:30:05 +1200

    Add Zap scan for DAST

---
+++ dast.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+
+case "${1}" in
+  test)
+    url='https://test-builddoctor-pipeline-playpen.azurewebsites.net/'
+  ;;
+
+  prod)
+    url='https//devops.nz'
+  ;;
+
+  *)
+    echo "Usage: $0 <env>"
+    exit 1
+  ;;
+esac
+
+docker run  -v $(pwd):/zap/wrk/:rw \
+  -t owasp/zap2docker-stable zap-full-scan.py \
+  -x zap.xml \
+  -st ${url}
