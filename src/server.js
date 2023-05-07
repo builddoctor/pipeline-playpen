238790b836d41a414b808e99420b3dccfed958a2
Julian Simpson <simpsonjulian@gmail.com>
2023-05-08 08:41:40 +1200

    Add a robots.txt

--- src/server.js
+++ src/server.js
@@ -4,3 +4,10 @@ const port = 8080
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+app.use(robots({
+  UserAgent: '*',
+  Allow: '/',
+  CrawlDelay: '1',
+  Sitemap: '/sitemap.xml',
+}))
