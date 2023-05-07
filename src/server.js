30818814c91c5052714115c9515b364727244cea
Julian Simpson <simpsonjulian@gmail.com>
2023-05-08 08:50:15 +1200

    Add sitemap

--- src/server.js
+++ src/server.js
@@ -1,10 +1,17 @@
 const app = require("./app");
 const robots = require("express-robots-txt")
+const expressSitemapXml = require('express-sitemap-xml')
 const port = 8080
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
+const getUrls = function() {
+  return ["/"];
+}
+
+app.use(expressSitemapXml(getUrls, 'https://test-builddoctor-pipeline-playpen.azurewebsites.net/'))
 
 app.use(robots({
   UserAgent: '*',
