583544b7e15af76a8844b981e1aadc7fcc357078
Julian Simpson <simpsonjulian@gmail.com>
2023-05-07 12:36:23 +1200

    Fix some of the DAST findings
    * Zap doesn't like some headers
    * This should reduce some of those findings

--- src/app.js
+++ src/app.js
@@ -7,7 +7,12 @@ const app = express()
 app.disable('x-powered-by');
 
 app.get('/', (req, res) => {
-    res.set('Content-Security-Policy', "default-src 'self'; img-src https://*; child-src 'none'");
+    res.set('Content-Security-Policy', "default-src 'self'; img-src 'self'; child-src 'none'");
+    res.set('Strict-Transport-Security', "Strict-Transport-Security: max-age=31536000; includeSubDomains");
+    res.set('X-Frame-Options', "X-Frame-Options: SAMEORIGIN");
+    res.set('X-Content-Type-Options', "X-Content-Type-Options: nosniff");
+    res.set('Referrer-Policy', "");
+    res.set('Permissions-Policy', "");
     res.send(message());
 
 })
--- tests/app.test.js
+++ tests/app.test.js
@@ -2,21 +2,35 @@ const request = require("supertest");
 const app = require("../src/app");
 
 describe("Test the root path", () => {
-    test("It should have a CSP", () => {
-        return request(app)
-            .get("/")
-            .then(response => {
 
-                expect(response.headers['content-security-policy'])
-                .toBe("default-src 'self'; img-src https://*; child-src 'none'");
-            });
-    });
+
+  it.each`
+    header                   | expectedValue
+
+    ${'content-security-policy'}        | ${"default-src 'self'; img-src 'self'; child-src 'none'"}
+    ${'strict-transport-security'}   | ${'Strict-Transport-Security: max-age=31536000; includeSubDomains'}
+    ${'x-frame-options'}    | ${'X-Frame-Options: SAMEORIGIN'}
+    ${'x-content-type-options'} | ${'X-Content-Type-Options: nosniff'}
+    ${'referrer-policy'} | ${''}
+    ${'permissions-policy'} | ${''}
+  `(
+    'should return "$expectedValue" when checking "$header"',
+    ({ header, expectedValue }) => {
+       return request(app)
+                .get("/")
+                .then(response => {
+                    expect(response.headers[header])
+                    .toBe(expectedValue);
+                });
+    },
+  );
+
+
 
     test("It should not advertise it's technology", () => {
         return request(app)
             .get("/")
             .then(response => {
-                console.log(response.headers)
                 expect(response.headers).toEqual(expect.not.objectContaining({'x-powered-by': 'Express'}));
             });
     });
