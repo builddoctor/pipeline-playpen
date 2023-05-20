aad55f33b16a4de2d189d0a4d58842268078eb85
Julian Simpson <simpsonjulian@gmail.com>
2023-05-21 11:13:16 +1200

    Factor out envs, add production

--- main.tf
+++ main.tf
@@ -110,64 +110,15 @@ resource "azurerm_app_service_plan" "test" {
   }
 }
 
-resource "azurerm_app_service" example {
-  name                = "test-builddoctor-pipeline-playpen"
-  app_service_plan_id = azurerm_app_service_plan.test.id
-  location            = azurerm_resource_group.test.location
-  resource_group_name = azurerm_resource_group.test.name
-  app_settings        = {
-    "DOCKER_REGISTRY_SERVER_URL"          = azurerm_container_registry.acr.login_server
-    "DOCKER_REGISTRY_SERVER_USERNAME"     = azurerm_container_registry.acr.admin_username
-    "DOCKER_REGISTRY_SERVER_PASSWORD"     = azurerm_container_registry.acr.admin_password
-    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
-  }
-  client_cert_mode = "Required"
-  https_only       = true
-  #  key_vault_reference_identity_id = "SystemAssigned"
-  logs {
-    detailed_error_messages_enabled = true
-    failed_request_tracing_enabled  = true
-
-    application_logs {
-      file_system_level = "Error"
-    }
-
-  }
-  site_config {
-
-    acr_use_managed_identity_credentials = false
-    always_on                            = false
-
-    dotnet_framework_version    = "v4.0"
-    ftps_state                  = "FtpsOnly"
-    http2_enabled               = true
-    ip_restriction              = []
-    linux_fx_version            = "DOCKER|builddoctorplaypen.azurecr.io/playpen:latest"
-    local_mysql_enabled         = false
-    managed_pipeline_mode       = "Integrated"
-    min_tls_version             = "1.2"
-    number_of_workers           = 1
-    remote_debugging_enabled    = false
-    scm_ip_restriction          = []
-    scm_type                    = "None"
-    scm_use_main_ip_restriction = false
-    use_32_bit_worker_process   = true
-    vnet_route_all_enabled      = false
-    websockets_enabled          = false
-  }
-
-
-  timeouts {}
-}
 
 resource "azuredevops_serviceendpoint_azurerm" "azure" {
 
-project_id = azuredevops_project.project.id
-service_endpoint_name = "Azure RM (TF)"
-description = "Managed by Terraform"
-azurerm_spn_tenantid = var.spn_tenant_id
-azurerm_subscription_id = var.subscription_id
-azurerm_subscription_name = var.subscription_name
+  project_id = azuredevops_project.project.id
+  service_endpoint_name = "Azure RM (TF)"
+  description = "Managed by Terraform"
+  azurerm_spn_tenantid = var.spn_tenant_id
+  azurerm_subscription_id = var.subscription_id
+  azurerm_subscription_name = var.subscription_name
 }
 
 # This resource doesn't support ACR right now.
