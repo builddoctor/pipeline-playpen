184f95ac442562dd6ad97ed986b3aaa6874a6898
Julian Simpson <simpsonjulian@gmail.com>
2023-05-20 15:34:14 +1200

    Refer to config via local url

--- dast.sh
+++ dast.sh
@@ -15,11 +15,11 @@ case "${1}" in
   ;;
 esac
 
-docker run  -v $(pwd):/zap/wrk/:rw \
+docker run  -v "$(pwd):/zap/wrk/:rw" \
    --user root \
   -t owasp/zap2docker-stable zap-baseline.py \
   -x zap.xml \
-  -u https://github.com/builddoctor/pipeline-playpen/blob/main/zap.conf \
+  -u file:///zap/wrk/zap.conf \
   -d \
   -t ${url}
 
